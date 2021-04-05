d85bd2ee8000813bfd95f8551ce2d9bacd10a0dc
Dineshkumar46 <81312564+Dineshkumar46@users.noreply.github.com>
2021-04-05 15:06:18 +0530

    Update BloodbankService_Login.js

--- react/src/Service/BloodbankService_Login.js
+++ react/src/Service/BloodbankService_Login.js
@@ -1,8 +1,6 @@
-
-//import React from "react";
 import axios from 'axios';
 
-const API_BASE_URL = "http://localhost:8080/";
+const API_BASE_URL = "https://8080-badacebeffecadfbbbbcadddaaafdfbabacbdeb.examlyiopb.examly.io/";
 
 class BloodbankService_Login   {
 
